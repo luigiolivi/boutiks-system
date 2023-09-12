9e02a459062d635867fb987a1deaa83fdd371851
luigiolivi <eterowiczluigi@gmail.com>
2023-09-12 20:13:50 -0300

    first commit

---
+++ src/Carrinho.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Carrinho {
+    ArrayList<Float> produtos = new ArrayList<>();
+    
+    public void adicionarProduto(float preco) {
+        
+    }
+    
+    public float calcularTotal() {
+        
+    }
+}
---
+++ src/Produtos.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Produtos {
+    String nome, descricao;
+    float preco;
+    
+    
+}
